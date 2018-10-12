5792fe7dbf16e7c5bcb844e68c76c89c9cbdaa22
minh <minhnguyengit@gmail.com>
2018-10-12 09:29:12 +0700

    Config with domain new judasfateblog.tk

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client'
 
 var api = {
-  local: 'https://209.97.186.216/blogserver',
+  local: 'https://judasfateblog.tk/blogserver',
   network: 'http://192.168.98.109:4000'
   // local: 'https://172.16.12.148:4000'
 }
